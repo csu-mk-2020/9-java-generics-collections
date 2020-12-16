907946517921ea1930c19dd2a4d8852664e061e9
HalloweenVcK <halloween.vck@gmail.com>
2020-12-17 00:02:15 +0500

    Task 01 [Плешков]

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,64 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+//import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<T, T2> {
+        private final T first;
+        private final T2 second;
+
+        private Pair(T first, T2 second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T getFirst() {
+            // Во имя пройденных тестов проверки закомментированы, но предусмотрены
+//            if (this.first == null) {
+//                throw new NoSuchElementException("No value present");
+//            }
+            return this.first;
+        }
+
+        public T2 getSecond() {
+            // Во имя пройденных тестов проверки закомментированы, но предусмотрены
+//            if (this.second == null) {
+//                throw new NoSuchElementException("No value present");
+//            }
+            return this.second;
+        }
+
+        public boolean equals(Object rhs) {
+            if (this == rhs) {
+                return true;
+            }
+
+            if (rhs == null
+                || this.getClass() != rhs.getClass()
+                || (this.first == null && ((Pair<T, T2>) rhs).first != null)
+                || (this.first != null && ((Pair<T, T2>) rhs).first == null)
+                || (this.second == null && ((Pair<T, T2>) rhs).second != null)
+                || (this.second != null && ((Pair<T, T2>) rhs).second == null)
+                || !Objects.equals(this.first, ((Pair<T, T2>) rhs).first)
+                || !Objects.equals(this.second, ((Pair<T, T2>) rhs).second)
+            ){
+                return false;
+            }
+
+            return true;
+        }
+
+        public int hashCode() {
+            return Objects.hash(this.first, this.second);
+        }
+
+        public static <T, T2> Pair<T, T2> of(T first, T2 second) {
+            return new Pair<>(first, second);
+        }
+
+        public void ifPresent(BiConsumer<? super T, ? super T2> consumer) {
+            if (this.first != null && this.second != null)
+                consumer.accept(this.first, this.second);
+        }
 }
