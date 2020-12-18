1da66e8334435c956f4b43a0cb625c55901bd8fc
Aleksey <alex-kadykov@bkru>
2020-12-18 08:35:25 +0500

    task02 [Кадыков]

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,14 @@
 package com.example.task03;
 
+import java.io.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Task03Main {
 
@@ -19,6 +22,31 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset));
+        Map<Integer, Set<String>> map = new HashMap<>();
+        bufferedReader.lines().filter(line -> line.length() > 3).forEach(line -> {
+            Stream<Character.UnicodeBlock> unicodeBlockStream = line.chars().mapToObj(Character.UnicodeBlock::of);
+            if (unicodeBlockStream.allMatch(str -> str.equals(Character.UnicodeBlock.CYRILLIC))) {
+                Set<String> set = map.computeIfAbsent(line.length(), key -> new HashSet<>());
+                set.add(line.toLowerCase());
+            }
+        });
+
+        Set<Set<String>> result = new HashSet<>();
+        map.forEach((key, set) -> set.forEach(str -> {
+            Set<String> tmp = new TreeSet<>();
+            int[] symbols = str.chars().sorted().toArray();
+            set.forEach(another -> {
+                if (Arrays.equals(symbols, another.chars().sorted().toArray())) {
+                    tmp.add(another);
+                }
+            });
+            if (tmp.size() > 1) {
+                result.add(tmp);
+            }
+        }));
+        List<Set<String>> sortResult = new ArrayList<>(result);
+        sortResult.sort(Comparator.comparing(str -> str.iterator().next()));
+        return sortResult;
     }
 }
